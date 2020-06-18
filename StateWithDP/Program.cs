f8eb3c2bee4747288e3bd71767a5c825c9b9dbaa
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-06-18 10:32:27 +0200

    State : with or without DP

---
+++ StateWithDP/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateWithDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Video video = new Video();
+            video.SetState(new VideoEnLecture());
+            video.Action();
+            video.SetState(new VideoEnPause());
+            video.Action();
+            video.SetState(new RetourAuDebut());
+            video.Action();
+            video.SetState(new SeRendreAuMilieu());
+            video.Action();
+
+            Console.ReadKey();
+        }
+    }
+    class Video
+    {
+        private IEtatVideo EtatVideo;
+
+        public void SetState(IEtatVideo newEtat)
+        {
+            this.EtatVideo = newEtat;
+        }
+
+        public void Action()
+        {
+            this.EtatVideo.Action();
+        }
+    }
+
+    public interface IEtatVideo
+    {
+        void Action();
+
+    }
+
+    public class VideoEnLecture : IEtatVideo
+    {
+        public void Action()
+        {
+            Console.WriteLine("La vidéo est en lecture");
+        }
+    }
+    public class VideoEnPause : IEtatVideo
+    {
+        public void Action()
+        {
+            Console.WriteLine("La vidéo est en pause");
+        }
+    }
+    public class RetourAuDebut : IEtatVideo
+    {
+        public void Action()
+        {
+            Console.WriteLine("La vidéo recommence depuis le début");
+        }
+    }
+    public class SeRendreAuMilieu : IEtatVideo
+    {
+        public void Action()
+        {
+            Console.WriteLine("Se Rendre Au Milieu De La Vidéo");
+        }
+    }
+}
---
+++ StateWithoutDP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Video video = new Video();
+
+            video.SetEtat("PLAY");
+            video.Action();
+
+            video.SetEtat("PAUSE");
+            video.Action();
+
+            video.SetEtat("DEBUT");
+            video.Action();
+
+            Console.ReadKey();
+        }
+    }
+    class Video
+    {
+        private string Etat = "";
+        public void SetEtat(string etat)
+        {
+            this.Etat = etat;
+        }
+
+        public void Action()
+        {
+            if (string.Equals(Etat, "PLAY", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("La vidéo est en lecture");
+            }
+            else if (string.Equals(Etat, "PAUSE", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("La vidéo est en pause");
+            }else if (string.Equals(Etat, "DEBUT", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("La vidéo recommence depuis le début");
+            }
+        }
+    }
+}
